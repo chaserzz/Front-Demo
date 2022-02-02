fa6efa4ab21be50ee7b9f066047dd9c22b56acb8
zard <467540052@qq.com>
2022-02-02 17:17:49 +0800

    事件bus代码优化-可实现先触发后订阅

--- JS/EventBus.js
+++ JS/EventBus.js
@@ -1,52 +1,117 @@
-class eventBus {
-  constructor(){
-    this.cache = {}  //创建一个对象用来保存
-  }
+var Event = (function (){
+  var _listen,
+      _trigger,
+      _remove,
+      _slice = Array.prototype.slice,
+      _shift = Array.prototype.shift,
+      _unshift = Array.prototype.unshift,
+      namespaceCache = {},
+      _create,
+      _default = 'default',
+      find,
+      // each 遍历数组
+      each = function (arr,fn){
+        var ret
+        for(var i = 0, l = arr.length; i < l; i++){
+          var n = arr[i];
+          ret = fn.call(n,i,n);
+        };
+        return ret;
+      };
+      // 封装增加监听函数
+      _listen = function(key,fn,cache){
+        if(!cache[key]){
+          cache[key] = []
+        }
+        cache[key].push(fn);
+      };
+      // 封装移除监听函数
+      _remove = function(key,cache,fn){
+        if(cache[key]){
+          if(fn){
+            for(var i = cache[key].length; i >= 0; i--){
+              if(cache[key][i] === fn){
+                cache[key].splice(i,1);
+                i--;
+              }
+            }
+          }else{
+            cache[key] = []
+          };
+        }
+      };
+      // 封装触发函数
+      _trigger = function(){
+        var cache = _shift.call(arguments), // 获得第一个参数为缓存
+            key = _shift.call(arguments) // 第二个参数为对应的监听事件的key
+            args = arguments, // 剩余参数
+            _self = this, // 保存当前调用的this
+            ret,
+            stack = cache[key];
+        // 当对应的listen为空或者没有监听事件时结束
+        if(!stack || !stack.length){
+          return;
+        }
+        return each(stack, function (){
+          console.log('this',this);
+          return this.apply(_self,args);
+        })
+      }
+      _create = function(namespace){
+        var namespace = namespace || _default; // 命名空间，防止事件名称重复导致的无法使用
+        var cache = {},
+            offlineStack = [], // 离线事件,即未发布就已经订阅的事件
+            ret = {
+              // 监听函数
+              listen: function(key,fn,last){
+                _listen(key,fn,cache);
+                if(offlineStack === null){
+                  return;
+                }
+                if(last === 'last'){
+                  // 只触发一个离线事件
+                  offlineStack.length && offlineStack.pop()();
+                }else{
+                  // 触发所有
+                  each(offlineStack, function(){
+                    // this?
+                    this();
+                  });
+                }
+              },
+              remove: function(key,fn){
+                _remove(key,cache,fn);
+              },
+              trigger: function(){
+                var fn,
+                    args,
+                    _self = this,
+                    args = _unshift.call(arguments,cache), //将cache作为第一个参数作为新的arguments
+                    fn = function (){
+                      return _trigger.apply(_self,args);
+                    };
+                    if(offlineStack){
+                      return offlineStack.push(fn);
+                    }
+                    return fn();
+              }
+            }
+            return namespace ? (namespaceCache[namespace] ? namespaceCache[namespace] : namespaceCache[namespace] = ret) : ret;
+      };
 
-  //创建on方法
-  on(name,fn){
-    //判断当前监听的事件名称是否已经存在
-    if(this.cache[name]){
-      this.cache[name].push(fn)
-    }else{
-      this.cache[name] = []
-      this.cache[name].push(fn)
+  return {
+    create: _create,
+    remove: function(key,fn){
+      var event = this.create();
+      event.remove(key,fn)
+    },
+    listen: function(key,fn,last){
+      var event = this.create();
+      event.listen(key,fn,last);
+    },
+    trigger: function(){
+      var event = this.create();
+      event.trigger.apply(this,arguments);
     }
-  }
-  
-  //创建off方法
-  off(name,fn){
-    let tasks = this.cache[name]
-    //是否存在该名称的事件
-    if(tasks){
-      let index = this.cache[name].findIndex(item => item === fn || item.callback === fn)  //item.callback === fn ?
-      if(index >= 0){
-        //找到对应的函数并消除
-        this.cache[name].splice(index,1)
-      }
-    }
-  }
-  
-  //创建emit方法
-  emit(name,once = false, ...args){
-    let tasks = this.cache[name]
-    if(tasks){
-      for(let fn of tasks){
-        fn(...args)
-      }
-      //仅仅发送一次函数
-      if(once){
-       this.cache[name] = null
-      }
-    }
-  }
-}
-
-const eventbus = new eventBus()
-const test = function (name){
-  console.log(`函数test触发:name=${name}`);
-}
-eventbus.on("test",test)
-//触发eventBus函数
-eventbus.emit("test",false,"trytrytry")
-eventbus.off("test",test)
+  };
+}());
--- index.js
+++ index.js
@@ -1,29 +1,19 @@
-/**
- * @param {number[]} nums1
- * @param {number} m
- * @param {number[]} nums2
- * @param {number} n
- * @return {void} Do not return anything, modify nums1 in-place instead.
- */
- var merge = function(nums1, m, nums2, n) {
-  let p1 = m - 1;
-  let p2 = n - 1;
-  let len = nums1.length - 1; 
-  while(p1 >= 0 && p2 >= 0){
-    if(nums1[p1] > nums2[p2]){
-      nums1[len] = nums1[p1]
-      p1 --
-      len--
-    }else{
-      nums1[len] = nums2[p2]
-      p2 --
-      len--
-    }
+let func = function (){
+  if(arguments.length === 0){
+
+  }else{
+    return arguments.callee;
   }
-  // p2还有剩余
-  if(p2 > 0){
-    for(let i = len; i >= 0; i--){
-      nums1[i] = nums2[i]
-    }
-  }
+}
+var curring = function (fn){
+  var args = [];
+  return func;
 };
+
+var demo = function (){
+  return;
+}
+
+var curDemo = curring(demo);
+var args = curDemo(1);
+console.log(args === func);
