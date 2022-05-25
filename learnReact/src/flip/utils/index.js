10658cc52407d3d18539f3217c0adadcda894cfc
chaserzz <467540052@qq.com>
2022-05-25 16:24:12 +0800

    flip compeleted

--- learnReact/src/flip/utils/index.js
+++ learnReact/src/flip/utils/index.js
@@ -3,11 +3,9 @@ export function calculateInvert(firstRect,lastRect){
 	const invert = {};
 	invert.scalX = firstRect.width  / lastRect.width;
 	invert.scalY = firstRect.height / lastRect.height;
-	const width = invert.scalX * lastRect.width / 2 * (invert.scalX > 1 ? 1 : -1);
-	const height = invert.scalY * lastRect.height / 2 * (invert.scalY > 1 ? 1 : -1);
-	console.log('first',firstRect,'last',lastRect,'width',width,'height',height);
+	const width =  lastRect.width * (invert.scalX - 1) / 2;
+	const height = lastRect.height * (invert.scalY - 1) / 2 ;
 	invert.left = (firstRect.left - lastRect.left + width);
 	invert.top = (firstRect.top - lastRect.top + height) ;
-	console.log('invert',invert)
 	return invert
 }
